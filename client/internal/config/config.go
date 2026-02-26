1bd9f17899e43c7dc356b4fffb6c1889f948825d
Gerhard Otto <gerrieotto123@gmail.com>
2026-02-26 20:30:59 +0200

    fix: Update config

--- client/internal/config/config.go
+++ client/internal/config/config.go
@@ -5,14 +5,17 @@ import "flag"
 // Config holds all CLI-configured values for the application.
 type Config struct {
 	Addr string
+	Name string
 }
 
 // Parse reads CLI flags and returns a populated Config.
 func Parse() *Config {
 	addr := flag.String("addr", "localhost:50051", "the address to connect to")
+	name := flag.String("name", "arthur", "name to greet")
 	flag.Parse()
 
 	return &Config{
 		Addr: *addr,
+		Name: *name,
 	}
 }
